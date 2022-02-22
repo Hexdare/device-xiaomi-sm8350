699af13405f49ef61c0cdc014351a387dab393e2
althafvly <althafvly@gmail.com>
2022-02-22 21:51:43 +0100

    sm8350-common: touch: add IHighTouchPollingRate
    Change-Id: I75e4bc4386eb5ade0fce2c37b2579875670dc9e3
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- common.mk
+++ common.mk
@@ -396,6 +396,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_sm8350
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
