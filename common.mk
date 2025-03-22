eb95b5e699e7b54e1a1ebf8db95371f72247c6ad
Hexdare <mohammedasimuddin786@gmail.com>
2025-03-22 16:03:05 +0530

    Revert "sm8350-common: touch: add IHighTouchPollingRate"
    This reverts commit 699af13405f49ef61c0cdc014351a387dab393e2.

--- common.mk
+++ common.mk
@@ -401,10 +401,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi_sm8350
-
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
